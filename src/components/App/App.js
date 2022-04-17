1556e3751aa55145c98849af0f507a263d5e2700
negmatullayeva <yildiznegmatullayeva@gmail.com>
2022-04-17 12:39:52 +0500

    add delete function

---
+++ src/components/App/App.js
@@ -0,0 +1,114 @@
+import React from "react";
+import AppHeader from "../AppHeader";
+import PostAddForm from "../PostAddForm";
+import PostList from "../PostList";
+import PostStatusFilter from "../PostStatusFilter";
+import SearchPanel from "../SearchPanel";
+
+import "./App.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          label: "Going to learn React JS",
+          important: false,
+          like: false,
+          id: "1",
+        },
+        { label: "That is so good", important: false, like: false, id: "2" },
+        { label: "I need a break...", important: false, like: false, id: "3" },
+      ],
+    };
+    this.maxId = 4;
+    this.deleteItem = this.deleteItem.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.onToggleImportant = this.onToggleImportant.bind(this);
+    this.onToggleLiked = this.onToggleLiked.bind(this);
+  }
+
+  deleteItem(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+
+      const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+      return {
+        data: newArr,
+      };
+    });
+  }
+
+  addItem(body) {
+    const newItem = {
+      label: body,
+      important: false,
+      id: this.maxId++,
+    };
+
+    this.setState(({ data }) => {
+      const newData = [...data, newItem];
+
+      return {
+        data: newData,
+      };
+    });
+  }
+
+  onToggleImportant(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const oldItem = data[index];
+      const newItem = { ...oldItem, important: !oldItem.important };
+
+      const newArr = [
+        ...data.slice(0, index),
+        newItem,
+        ...data.slice(index + 1),
+      ];
+      return {
+        data: newArr,
+      };
+    });
+  }
+
+  onToggleLiked(id) {
+    this.setState(({ data }) => {
+      const index = data.findIndex((elem) => elem.id === id);
+      const oldItem = data[index];
+      const newItem = { ...oldItem, like: !oldItem.like };
+
+      const newArr = [
+        ...data.slice(0, index),
+        newItem,
+        ...data.slice(index + 1),
+      ];
+      return {
+        data: newArr,
+      };
+    });
+  }
+
+  render() {
+    const liked = this.state.data.filter((item) => item.like).length;
+    const allPosts = this.state.data.length;
+    return (
+      <div className="app">
+        <AppHeader liked={liked} allPosts={allPosts} />
+        <div className="seach-panel d-flex">
+          <SearchPanel />
+          <PostStatusFilter />
+        </div>
+        <PostList
+          posts={this.state.data}
+          onDelete={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleLiked={this.onToggleLiked}
+        />
+        <PostAddForm onAdd={this.addItem} />
+      </div>
+    );
+  }
+}
---
+++ src/components/PostStatusFilter/PostStatusFilter.js
@@ -0,0 +1,9 @@
+const PostStatusFilter = () => {
+  return (
+    <div className="btn-group">
+      <button className="btn btn-info">All</button>
+      <button className="btn btn-outline-secondary">Liked</button>
+    </div>
+  );
+};
+export default PostStatusFilter;
